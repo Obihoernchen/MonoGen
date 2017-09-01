96ba2ed7e564b4c808837714fda011b3bb10c25b
Markus Hilger <obihoernchende@gmail.com>
2017-09-02 00:02:35 +0200

    Add proxy support for chrome webdriver

--- monogen/accountcreator.py
+++ monogen/accountcreator.py
@@ -89,7 +89,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
     if captchakey2:
         # TODO add proxy support for PhantomJS
         dcap = dict(DesiredCapabilities.PHANTOMJS)
-        dcap["phantomjs.page.settings.userAgent"] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
+        dcap["phantomjs.page.settings.userAgent"] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12127_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
         driver = PhantomJS(desired_capabilities=dcap)
     else:
         chrome_options = webdriver.ChromeOptions()
@@ -119,6 +119,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
     user.clear()
     user.send_keys(username)
 
+    """
     _validate_username(driver, username)
 
     elem = driver.find_element_by_name("password")
@@ -198,7 +199,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
     print("Account successfully created.")
     driver.quit()
     return True
-
+    """
 
 def _validate_response(driver):
     url = driver.current_url
